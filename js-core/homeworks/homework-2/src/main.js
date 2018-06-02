4eb4d43a6f9709d65a1a62ffaa53f6307e4bbabe
lepekhovandrei <lepekhov.andrei@gmail.com>
2018-06-02 12:54:28 +0300

    second commit hw_2

---
+++ js-core/homeworks/homework-2/src/main.js
@@ -0,0 +1,44 @@
+// const userPhoto = NaN;
+// const userName = 'Egor';
+// const userSecondName = 'Gudkov';
+// const userCompany = 'MasGroup';
+// const userEmail = 'egorgudkov@gmail.com';
+// const userLove = Infinity; // =)
+
+// const userMainInfo = [
+//     userPhoto,
+//     userName,
+//     userSecondName,
+//     userCompany
+// ]
+
+// const userPhoneAndMessenger = {
+//     phone: {
+//         firstNum: +380671112233,
+//         secondNum: undefined
+//     },
+//     messenger: {
+//         viber: null,
+//         telegram: +380672223344
+//     }
+// }
+
+
+
+// const userAnotherInfo = {
+//     email: userEmail,
+//     lovePerson: userLove
+// }
+
+// const contact = [
+//     userMainInfo,
+//     userPhoneAndMessenger,
+//     userAnotherInfo
+// ]
+
+// console.log(contact);
+
+
+
+// HOME WORK 2
+
