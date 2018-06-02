736eee3c2db532108a604bba7df160bfb5da9075
lepekhovandrei <lepekhov.andrei@gmail.com>
2018-06-02 15:10:27 +0300

    homework_2

--- js-core/homeworks/homework-2/src/main.js
+++ js-core/homeworks/homework-2/src/main.js
@@ -1,44 +1,79 @@
-// const userPhoto = NaN;
-// const userName = 'Egor';
-// const userSecondName = 'Gudkov';
-// const userCompany = 'MasGroup';
-// const userEmail = 'egorgudkov@gmail.com';
-// const userLove = Infinity; // =)
+/*
+ 1. Составить предложение из нижестоящих переменных:
+ "Сколько нужно программистов чтобы сделать проект ? 1, 25, команда"
+ */
 
-// const userMainInfo = [
-//     userPhoto,
-//     userName,
-//     userSecondName,
-//     userCompany
-// ]
+let numbers = [25, 1];
+let project = 'проект';
+let team = `команда`;
+let howMuch = 'Сколько';
+let sentence = 'нужно программистов чтобы сделать проект ?';
 
-// const userPhoneAndMessenger = {
-//     phone: {
-//         firstNum: +380671112233,
-//         secondNum: undefined
-//     },
-//     messenger: {
-//         viber: null,
-//         telegram: +380672223344
-//     }
-// }
+// --- Решение
+
+// let task1 = howMuch + sentence + ' ' + numbers[1] + ', ' + numbers[0];
+let task1 = `${howMuch} ${sentence}  ${numbers[1]}, ${numbers[0]}`;
 
 
-
-// const userAnotherInfo = {
-//     email: userEmail,
-//     lovePerson: userLove
-// }
-
-// const contact = [
-//     userMainInfo,
-//     userPhoneAndMessenger,
-//     userAnotherInfo
-// ]
-
-// console.log(contact);
+console.log(task1);
 
 
+/*
+ 2. Составьте предложение из представленного массива
+ и выведите предложение в консоль;
+ "Так, когда Будда достиг Просветления, он обнаружил, что больше не ощущает себя мишенью.
+  Он не был больше ни телом, к которому рано или поздно ..."
+  // index +++
+ */
 
-// HOME WORK 2
+let array = [
+    'Он',
+    'был больше ни телом, к которому рано или поздно',
+    'он обнаружил',
+    'не',
+    'Так, когда Будда достиг Просветления',
+    '...',
+    'что больше',
+    'ощущает себя мишенью',
+    'не'
+  ];
 
+  // --- Решение
+  
+  let homeSentence = '"' + array[4] + ', ' + array[2] + ', ' + array[6] + ' ' + array[8] + 
+  array[7] + '.' + '\n' + array[0] + ' ' + array[3] + ' ' + array[1] + ' ' + array[5] + '"'
+
+console.log(homeSentence);
+
+
+/*
+ 3. Добавьте свойста 4 новых свойства в объект используя
+    квадратные скобки и точку.
+ */
+
+
+// --- Решение
+
+let myObj = {
+    name: 'Andrei'.toLocaleUpperCase(),
+    growth: ' 179.5 '.trim(),
+    replaceName: 'Andrei'.replace('i', 'y'),
+    firstNameWord: 'Andrei'[0],
+    lenghtName: 'Andrei'.length
+};
+
+console.log(myObj);
+
+/*
+ 5. Преобразуйте строку x,
+    в максимально удобно читаемый для программиста вид
+ */
+
+let frameworks = [4.7, 'Angular', '6Angular', 'React/Redux'];
+let x = 
+        'google \
+        released ' + 'new version\
+        ' + frameworks[1] + Math.floor(frameworks[0]) + '\
+        But real speed is ' + `${frameworks[frameworks.length - 1]}`;
+
+  console.log(x)
