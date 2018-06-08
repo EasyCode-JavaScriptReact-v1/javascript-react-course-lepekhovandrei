2587382fc98eec75b9586478f27ca4be3725580f
lepekhovandrei <lepekhov.andrei@gmail.com>
2018-06-08 15:17:41 +0300

    new commit

--- js-core/homeworks/homework-3/src/main.js
+++ js-core/homeworks/homework-3/src/main.js
@@ -42,6 +42,12 @@ const objectWithName = { name: 'privet kak dela' };
 console.log(toUpperCase(objectWithName));
 
 
+
+
+
+
+
+//-------------------------------------------------------
 /*
 1. Напишите функцию которая принимает в
 качестве аргумента массив и элемент массива
@@ -62,3 +68,59 @@ function addToArray(arrParam, newElem) {
 addToArray(arr, 34);
 addToArray(arr, 55);
 console.log(arr);
+
+//-------------------------------------------------------
+
+
+/*
+2. Напишите функцию которая получает 3 параметра
+и возвращает объект типа:
+    {
+      argument1: param1,
+      argument2: param2,
+      argument3: param3,
+    }
+*/
+
+function simpleObjectGenerator(one, two, three) {
+  const obj = {
+    arg1: one,
+    arg2: two,
+    arg3: three
+  };
+
+  return obj;
+}
+
+console.log(simpleObjectGenerator('protocol', { url: '22' }, 8000));
+
+
+
+/*
+3.  Напишите функцию которая принимает 3 аргумента,
+     третий аргумент - это объект.
+    Функция создает новый объект и добавляет ключ
+    это первый аргумент, а значение - второй аргумент
+    Проверяет если есть свойство name в переданном объекте,
+    тогда добавляет данное свойство и возвращает новый объект
+*/
+
+
+
+var myName = { name: 'Andrei' };
+
+function addNameToUser(newKey, newValue, userName) {
+  let newObj = {};
+
+  newObj[newKey] = newValue;
+
+  if (userName.name) {
+    newObj.name = userName
+  }
+  return newObj;
+}
+
+console.log(addNameToUser('family', '%Lepekhov%', myName));
+console.log(addNameToUser('name', 'privet Pasha', {}));
+
+console.log(myName);
